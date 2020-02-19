55eee029ca5abc7cdd9f6a0826bd0778bb97f835
Rance Jen <rance_jen@trendmicro.com>
2020-02-19 13:31:57 +0800

    Update route

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
     
-@app.route("/salvador")
-def salvador():
-    return "Hello, Salvador"
+@app.route("/set_budget", methods=['POST'])
+def set_budget():
+    return "Set budget successfully"
     
 if __name__ == "__main__":
     app.run(debug=True)
